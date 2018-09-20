d8f8bd615fce148c454bcdb5d5702ffe579f7502
zhouxh <cn.yu.zhou@gmail.com>
2018-09-20 15:12:39 +0800

    FacadePattern
    Change-Id: I6258f0734c602ec6a304a23fda01f6b874a47f49

---
+++ DesignPatternTest/src/FacadePattern/ShapeMaker.java
@@ -0,0 +1,25 @@
+package FacadePattern;
+
+public class ShapeMaker {
+	private Shape rectangle;
+	private Shape circle;
+	private Shape square;
+	
+	public ShapeMaker() {
+		circle=new Circle();
+		rectangle=new Rectangle();
+		square=new Square();
+	}
+	
+	public void drawCircle() {
+		circle.draw();
+	}
+	
+	public void drawRectangle() {
+		rectangle.draw();
+	}
+	
+	public void drawSquare() {
+		square.draw();
+	}
+}
---
+++ DesignPatternTest/src/FacadePattern/Square.java
@@ -0,0 +1,9 @@
+package FacadePattern;
+
+public class Square implements Shape {
+ 
+   @Override
+   public void draw() {
+      System.out.println("Square::draw()");
+   }
+}
