9e1ddbc709ba294e9f425ca8ea9492beba87f258
zhouxh <cn.yu.zhou@gmail.com>
2018-09-20 14:58:01 +0800

    DecoratorPattern
    Change-Id: I139cd3caf7460161e5eb63e6e0f922885ecdc99e

---
+++ DesignPatternTest/src/DecoratorPattern/Rectangle.java
@@ -0,0 +1,11 @@
+package DecoratorPattern;
+
+public class Rectangle implements Shape {
+
+	@Override
+	public void draw() {
+		// TODO Auto-generated method stub
+		System.out.println("Shape: Rectangle");
+	}
+
+}
---
+++ DesignPatternTest/src/DecoratorPattern/ShapeDecorator.java
@@ -0,0 +1,16 @@
+package DecoratorPattern;
+
+public abstract class ShapeDecorator implements Shape {
+	protected Shape decoratedShape;
+	
+	public ShapeDecorator(Shape decoratedShape) {
+		this.decoratedShape=decoratedShape;
+	}
+	
+	@Override
+	public void draw() {
+		// TODO Auto-generated method stub
+		decoratedShape.draw();
+	}
+
+}
